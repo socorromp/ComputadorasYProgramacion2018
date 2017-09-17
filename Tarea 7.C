4bf6255193acfb591cea7f30a78c20e9f28af579
Socorro Montoya <31621073+socorromp@users.noreply.github.com>
2017-09-17 00:47:32 -0500

    Create Tarea 7.C

---
+++ Tarea 7.C
@@ -0,0 +1,43 @@
+/*
+María del Socorro Montoya Pérez 
+Computadoras y Programación
+Programa de comparación de 3 números y cuál es el mayor.
+Tarea #7
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+int num1=0;
+int num2=0;
+int num3=0;
+ 
+ printf("Dame el primer número:");
+ scanf("%d", & num1);
+ printf("Dame el segundo número:");
+ scanf("%d",& num2);
+ printf("Dame el tercer número:");
+ scanf("%d",& num3);
+ 
+ if(num1>0 && num2>0 && num3>0 && (num1!=num2) && (num1!=3) && (num2!=3))
+ {
+           if (num1>num2 && num1>num3) {
+                          printf("%d es el mayor\n", num1);
+                          }
+           if (num2>num1 && num2>num3) {
+                          printf("%d es el mayor\n", num2);
+                          }
+           if (num3>num1 && num3>num2) {
+                          printf("%d es el mayor\n", num3);
+                          }
+} else{
+
+ printf("Los numeros deben ser mayores que 0 y diferentes entre sí.\n");
+}
+  system("PAUSE");
+  	
+
+}
