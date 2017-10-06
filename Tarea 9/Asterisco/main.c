f98a5565628e853467694a42144d53fa6241a0a6
Socorro Montoya <31621073+socorromp@users.noreply.github.com>
2017-10-06 13:19:01 -0500

    Add files via upload

---
+++ Tarea 9/Asterisco/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int m=0, i=0;
+	
+	printf ("Ingresa un numero: ");
+	scanf("%d",&i);
+
+		for (m=0; m<i; m++)
+			printf("*\n");
+			
+  printf("\n");
+  system("PAUSE");	
+  return 0;
+}
