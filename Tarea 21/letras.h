9453d416a854fd9d7ad3f82bcaeb3e8d9a6c20cc
Socorro Montoya <31621073+socorromp@users.noreply.github.com>
2017-11-12 22:45:25 -0600

    Add files via upload

---
+++ Tarea 21/letras.h
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void fnM(){
+
+printf("M     M\n");
+printf("MM   MM\n");
+printf("M M M M\n");
+printf("M  M  M\n");
+printf("M     M\n");
+}
+void fnA(){
+    printf("    A\n");
+	printf("   A A\n");
+	printf("  A   A\n");
+	printf(" A A A A\n");
+	printf("A       A\n\n");
+	
+}
+void fnR(){
+
+	printf("RRRRRR\n");
+    printf("R    R\n");
+    printf("RRRRR\n");
+    printf("R    R  \n");
+    printf("R     R \n");
+}
+void fnI()
+{
+	
+	printf("IIIIIII\n");
+	printf("   I  \n");
+	printf("   I  \n");
+	printf("   I  \n");
+	printf("IIIIIII\n\n");
+}
---
+++ Tarea 21/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "letras.h"
+
+int main(int argc, char *argv[])
+{
+
+	fnM();
+	    printf("\n");
+	fnA();
+		printf("\n");
+	fnR();
+		printf("\n");
+	fnI();
+		printf("\n");
+	fnA();
+  system("PAUSE");	
+  return 0;
+}
